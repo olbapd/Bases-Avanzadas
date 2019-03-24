cf3e43ff5d6c3f2072c08acb8d54255de39831b8
JoseRIvera07 <ce.joserivera7@gmail.com>
2019-03-23 19:08:58 -0600

    Boton Registrar, Modificar y llenado de Dropdown Estado

--- Proyecto1/Frontend/src/app/auth/manage-assets/manage-assets.component.ts
+++ Proyecto1/Frontend/src/app/auth/manage-assets/manage-assets.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Estado} from '../manage-assets/Estado';
+import {RestApiService} from 'src/app/rest_client/client_service';
 
 @Component({
   selector: 'app-manage-assets',
@@ -6,10 +8,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./manage-assets.component.css']
 })
 export class ManageAssetsComponent implements OnInit {
+  estado:Estado[];
+  
 
-  constructor() { }
+  constructor(public restApi: RestApiService) {
+   }
 
   ngOnInit() {
+    this.estado =  this.restApi.getEstado(); 
+    
+  }
+
+  registrar_activo(nombre,descripcion,fecha_compra,precio_compre,valor_residual,detalle_ubicacion,codigo,categoria,fecha_registro,tiempo_garantia,vida_util,centro_costo,estado){
+    console.log(estado);
+  }
+
+  modificar_estado_activo(activo,estado_activo){
+    console.log(activo);
   }
 
 }
