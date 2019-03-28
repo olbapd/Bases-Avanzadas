da5e82d73c4db1136f719acf6406d4e8193f07c6
Kenneth Jeanpol Alvarado Mendez <ce.kalvarado@gmail.com>
2019-03-28 02:29:40 -0600

    minor fixes

--- Proyecto1/Frontend/src/app/admin_view/crud_employee/crud_employee.component.ts
+++ Proyecto1/Frontend/src/app/admin_view/crud_employee/crud_employee.component.ts
@@ -3,11 +3,9 @@ import { Router } from '@angular/router';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { RestApiService } from 'src/app/rest_client/client_service';
 import { HttpClient } from '@angular/common/http';
-import { Estado } from './../manage-assets/Estado';
 import { sede } from '../../interfaces/sede';
 import { department } from '../../interfaces/department';
 import { jobs } from '../../interfaces/jobs';
-import { Categoria } from '../manage-assets/Categoria';
 import { asset } from 'src/app/interfaces/assets_Structure';
 import { Empleado } from 'src/app/interfaces/employee';
 import {MatDialog, MatDialogConfig} from '@angular/material';
@@ -22,8 +20,8 @@ import { DeleteComponent } from '../dialogs/delete_confirm/delete_confirm.compon
 
 export class EmployeeComponent implements OnInit {
     isPopupOpened=false;
-    sede: sede[];
-    estado: Estado[];
+    sede;
+    estado;
     puestos: jobs[];
     departamento: department[];
     selectedFile: File = null;
@@ -36,7 +34,7 @@ export class EmployeeComponent implements OnInit {
         puesto:"asd",
     }]
 
-    categoria: Categoria[];
+    categoria;
     page = 1;
     pageSize = 4;
     activos: asset[] = [{
--- Proyecto1/Frontend/src/app/admin_view/crud_sede/crud_sede.component.ts
+++ Proyecto1/Frontend/src/app/admin_view/crud_sede/crud_sede.component.ts
@@ -7,7 +7,6 @@ import { Provincia } from 'src/app/interfaces/provincia';
 import { Canton } from 'src/app/interfaces/canton';
 import { Distrito } from 'src/app/interfaces/distrito';
 import { Empleado } from 'src/app/interfaces/employee';
-import { Categoria } from '../manage-assets/Categoria';
 import { asset } from 'src/app/interfaces/assets_Structure';
 import { sede } from 'src/app/interfaces/sede';
 import { MatDialog, MatPaginator, MatSort, MatDialogConfig } from '@angular/material';
@@ -29,7 +28,7 @@ export class SedeComponent implements OnInit {
     empleado: Empleado[];
 
     type = 1;
-    categoria: Categoria[];
+    categoria;
     page = 1;
     pageSize = 4;
 
