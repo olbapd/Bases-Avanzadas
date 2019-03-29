850fdcbd634d8b6a0705e8727da8cc02271bf80a
JoseRIvera07 <ce.joserivera7@gmail.com>
2019-03-28 22:34:19 -0600

    LogIn acoplado con popUp de error

--- Proyecto1/Frontend/src/app/config/constants.ts
+++ Proyecto1/Frontend/src/app/config/constants.ts
@@ -1,6 +1,7 @@
 export class urls{
 public static urlbase:string = 'http://localhost:3000/api/';
 public static sp_url:string = 'dynamic/'; //activo
+public static auth_url:string ='authentication/login';
 
 public static activo_url:string = 'activo/'; //funcion
 
