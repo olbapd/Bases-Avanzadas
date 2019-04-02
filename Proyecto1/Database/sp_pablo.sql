6c81269b6561facb232e09bc476d3b43a3df408e
olbapd <pablod.garciab@gmail.com>
2019-04-01 23:06:50 -0600

    Assign active sp update.

--- Proyecto1/Database/sp_pablo.sql
+++ Proyecto1/Database/sp_pablo.sql
@@ -10,7 +10,7 @@ AS
 GO  
 
 
-GO  
+/*GO  
 CREATE OR ALTER PROCEDURE sp_assignActive   
     @IdEmpleado int,   
     @IdActivo int   
@@ -19,4 +19,48 @@ AS
     UPDATE Proyecto1BDA.dbo.Activo 
 	SET IdEmpleado= @IdEmpleado, FechaAsignacion = GETDATE()
 	WHERE IdActivo = @IdActivo;
-GO 
+GO*/ 
+
+CREATE OR ALTER PROC [dbo].sp_assignActive
+	@Codigo varchar(50),
+	@Cedula varchar(50),
+	@IdEstado int,
+	@DetalleUbi varchar(100),
+	@Correo varchar(50) OUTPUT,
+	@Nombre varchar(50) OUTPUT,
+	@Apellido varchar(50) OUTPUT
+
+	
+AS
+BEGIN
+	DECLARE
+	@IdSede int,
+	@IdEmpleado int
+
+	SELECT @IdEmpleado = Empleado.IdEmpleado, @Correo=Empleado.Correo,
+			@Nombre=Empleado.Nombre, @Apellido=Empleado.Apellido1 FROM Empleado
+	WHERE @Cedula = [Empleado].Cedula;
+
+	SELECT @IdSede = SedeXEmpleado.IdSede FROM SedeXEmpleado
+	WHERE @IdEmpleado = [SedeXEmpleado].IdEmpleado;
+
+	BEGIN TRAN
+	BEGIN TRY
+		UPDATE Activo SET 
+		[IdEstado] = @IdEstado,
+		[IdEmpleado] = @IdEmpleado,
+		[IdSede] = @IdSede,
+		[DetalleUbicacion]= @DetalleUbi,
+		[FechaAsignacion] = GETDATE()
+		WHERE @Codigo = [Activo].Codigo
+		COMMIT TRANSACTION
+	END TRY
+	BEGIN CATCH
+		SELECT ERROR_PROCEDURE() AS ErrorProcedimiento, ERROR_MESSAGE() AS TipoError
+		ROLLBACK TRANSACTION
+	END CATCH
+	SELECT	@Correo as N'@Correo',
+		@Nombre as N'@Nombre',
+		@Apellido as N'@Apellido'
+END
+GO
