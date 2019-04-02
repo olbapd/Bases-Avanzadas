4384dacd12d02f04869cb6798b350fdd9d530bb6
olbapd <pablod.garciab@gmail.com>
2019-04-02 00:38:50 -0600

    Failover to another server implemented. Not tested.

--- Proyecto1/Backend/src/repository/db.js
+++ Proyecto1/Backend/src/repository/db.js
@@ -2,7 +2,7 @@
 'use strict';
 
 let sql = require('mssql');
-let config = require('config');
+let sqlServer = require('../tools/sqlserver');
 
 let expose = {
   storedProcedure  : undefined,
@@ -15,8 +15,80 @@ expose.storedProcedure = (data, cb) => {
     detail : null,
     data: null
   };
-  
-  sql.connect(config.get('sqlserver2')).then(pool => {
+  let servers = sqlServer.getServers();
+  let i =0;
+  let lastError="";
+  while(i<servers.length){
+    try{
+      sql.connect(servers[i]).then(pool => {
+      
+        let request = pool.request();
+        let typesIn = data.typesIn;
+        let typesOut = data.typesOut;
+        let inputs = data.inputs;
+        let values = data.values;
+        let outputs = data.ouputs;
+        let spName = data.name;
+        for(let i =0;i<inputs.length;i++){
+          if(typesIn[i]=="int"){
+            request.input(inputs[i],sql.Int,values[i]);
+          }
+          else if(typesIn[i]=="varchar"){
+            request.input(inputs[i],sql.NVarChar,values[i]);
+          }
+          else if(typesIn[i]=="date"){
+            request.input(inputs[i],sql.DateTime,values[i]);
+          }
+          else{
+            global.log4us.error('Error on building sp: '+spName);
+          }
+        }
+
+        for(let i =0;i<outputs.length;i++){
+          if(typesOut[i]=="int"){
+            request.input(outputs[i],sql.Int);
+          }
+          else if(typesOut[i]=="varchar"){
+            request.input(outputs[i],sql.NVarChar);
+          }
+          else if(typesOut[i]=="date"){
+            request.input(outputs[i],sql.DateTime);
+          }
+          else{
+            global.log4us.error('Error on building sp: '+spName);
+          }
+        }
+        
+        request.execute(spName , (err,result) => {
+          if (err) {
+            global.log4us.error('Error on sp ('+spName+'):'+err);
+            to_return.error = true;
+            to_return.success = false;
+            to_return.detail = err;
+            cb(to_return);
+            return;
+          }
+          to_return.success = true;
+          try{
+            to_return.data=result.recordset;        
+          }
+          catch(err){
+            to_return.data=result;        
+          }
+          
+          cb(to_return);
+          return;
+        });
+      });
+      return;
+    }catch(error){
+      lastError=error;
+      i++
+    }
+    global.log4us.error(`Could not create a connection to any database server. Error ${lastError}`);  
+  }
+
+  /*sql.connect(config.get('sqlserver2')).then(pool => {
     
     let request = pool.request();
     let typesIn = data.typesIn;
@@ -73,7 +145,7 @@ expose.storedProcedure = (data, cb) => {
       
       cb(to_return);
     });
-  });
+  });*/
 }
 
 module.exports = expose;
--- Proyecto1/Backend/src/tools/sqlserver.js
+++ Proyecto1/Backend/src/tools/sqlserver.js
@@ -12,24 +12,45 @@
 
 let config = require('config');
 let currentConfig = config.get('sqlserver');
+let servers = config.get('sqlservers');
 const sqlserver = require('mssql');
 
 let pool = null;
 
 let instance = {
   getPool : null,
+  getServers: null
 }
 
 let initializePool = () => {
   if(!pool){
-    global.log4us.print(`Creating connection to database server (${currentConfig.server})`);
+    let i =0;
+    while(i<servers.length){
+    //for (let i = 0; i < servers.length; i++) {
+      try{
+        global.log4us.print(`Creating connection to database server (${servers[i].server})`);
+        pool = new sqlserver.ConnectionPool(servers[i]);
+        pool.connect(err =>{
+          if (err!=null){
+            global.log4us.error(`Error creating connection to database server (${servers[i].server}), retrying conection...`);  
+            console.log(err);
+          }
+        });
+        return;
+      }catch(error){
+        i++;
+      }
+    }
+    global.log4us.error(`Could not create a connection to any database server.`);  
+            
+    /*global.log4us.print(`Creating connection to database server (${currentConfig.server})`);
     pool = new sqlserver.ConnectionPool(currentConfig);
     pool.connect(err =>{
       if (err!=null){
         global.log4us.error(`Error creating connection to database server (${currentConfig.server}), retrying conection...`);  
         console.log(err);
       }
-    });
+    });*/
   }
 }
 
@@ -37,6 +58,10 @@ instance.getPool = () => {
   initializePool();
   return pool;  
 }
+
+instance.getServers= () =>{
+  return servers;
+}  
 
   
 module.exports = instance;
