bf065a7fb2b195c2ce481171a62d76ae5f2795cc
olbapd <pablod.garciab@gmail.com>
2019-04-03 15:27:26 -0600

    Fix to date insertion.

--- Proyecto1/Backend/src/controllers/dynamic.js
+++ Proyecto1/Backend/src/controllers/dynamic.js
@@ -23,6 +23,78 @@ const sqlserver = require('mssql');
 let router = module.exports = express.Router();
 
 let currentConfig = config.get('sqlserver');
+/*
+let getSp = (req,res) => {
+     
+    let servers = sqltool.getServers();
+        let i =0;
+
+    while(i<servers.length){
+      try{
+        if(sqltool.getPool().connected){
+          sqltool.getPool().close();
+        }
+        sqlserver.connect(servers[i]).then(pool => {
+    
+        //let request =  new sqlserver.Request(sqltool.getPool());
+        let request =  new pool.Request();
+        let typesIn = req.body.typesIn;
+        let typesOut = req.body.typesOut;
+        let inputs = req.body.parameters;
+        let values = req.body.values;
+        let outputs = req.body.ouputs;
+        let spName = req.body.name;
+        for(let i =0;i<inputs.length;i++){
+          if(typesIn[i]=="int"){
+            request.input(inputs[i],sqlserver.Int,values[i]);
+          }
+          else if(typesIn[i]=="varchar"){
+            request.input(inputs[i],sqlserver.NVarChar,values[i]);
+          }
+          else if(typesIn[i]=="date"){
+            request.input(inputs[i],sqlserver.DateTime,values[i]);
+          }
+          else{
+            global.log4us.error('Error on building sp: '+spName);
+          }
+        }
+
+        for(let i =0;i<outputs.length;i++){
+          if(typesOut[i]=="int"){
+            request.input(outputs[i],sqlserver.Int,null);
+          }
+          else if(typesOut[i]=="varchar"){
+            request.input(outputs[i],sqlserver.NVarChar,null);
+          }
+          else if(typesOut[i]=="date"){
+            request.input(outputs[i],sqlserver.DateTime,null);
+          }
+          else{
+            global.log4us.error('Error on building sp: '+spName);
+          }
+        }
+        request.execute(spName, (err, recordset) => {
+          if (err) {
+            global.log4us.error(`Error getting ${spName}: ${err}`);
+            res.json({
+                success : false,
+                error: err
+            });
+            return;
+          }
+          res.status(200).json({
+            success : true,
+            data: recordset.recordset
+          });
+        });
+        });
+      }
+      catch(err){
+         console.log(err);
+         i++;
+      }
+    }  
+}*/
 
 let getSp = (req,res) => {
      
@@ -50,7 +122,8 @@ let getSp = (req,res) => {
         request.input(inputs[i],sqlserver.NVarChar,values[i]);
       }
       else if(typesIn[i]=="date"){
-        request.input(inputs[i],sqlserver.DateTime,values[i]);
+        let yourDate = new Date(values[i]);
+        request.input(inputs[i],sqlserver.DateTime,yourDate);
       }
       else{
         global.log4us.error('Error on building sp: '+spName);
@@ -86,7 +159,6 @@ let getSp = (req,res) => {
       });
     });
 }
-
 router.post('/*', (req, res) => {
   getSp(req,res);
 });
@@ -112,5 +184,4 @@ router.post('/*', (req, res) => {
     res.json(result);
 
   })
-  
 });*/
