1ded7ad2a0d1895f81fb9c29731dac5ccc58ada7
Kenneth Jeanpol Alvarado Mendez <ce.kalvarado@gmail.com>
2019-04-07 23:32:34 -0600

    Second-Method Fix

--- Proyecto1/Frontend/src/app/admin_view/dialogs/second_method/second-method.component.ts
+++ Proyecto1/Frontend/src/app/admin_view/dialogs/second_method/second-method.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Inject } from '@angular/core'
+import {Component, OnInit, Inject} from '@angular/core'
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { RestApiService } from 'src/app/services/client_service';
 import { Router } from '@angular/router';
@@ -6,93 +6,39 @@ import { MatDialogRef, MAT_DIALOG_DATA, MatDialog } from '@angular/material';
 import { updateComponent } from '../update_employee/update-employee.component';
 import { Calculo } from 'src/app/interfaces/calculo';
 import { Depreciation } from 'src/app/services/depreciation';
-import { sreporte } from 'src/app/interfaces/sreporte';
-@Component({
-    selector: 'second-method',
-    templateUrl: './second-method.component.html',
-    styleUrls: ['./second-method.component.css']
-})
+@Component ({
+    selector:'second-method',
+    templateUrl:'./second-method.component.html',
+    styleUrls:['./second-method.component.css']
+    })
 
-export class SecondMethodComponent implements OnInit {
+export class SecondMethodComponent implements OnInit{
 
 
-    page = 1;
-    pageSize = 4;
-    calculos: sreporte[] = [];//debe de inicializarse de lo contrario muestra vacio
+    calculos : Calculo[]=[];//debe de inicializarse de lo contrario muestra vacio
     valores;
-    constructor(public calcular: Depreciation, private modalService: NgbModal, public restApi: RestApiService,
-        private router: Router, private dialogRef: MatDialogRef<updateComponent>, @Inject(MAT_DIALOG_DATA) public data: any) {
+    constructor(public calcular: Depreciation,private modalService: NgbModal, public restApi: RestApiService, 
+        private router: Router,private dialogRef: MatDialogRef<updateComponent>, @Inject(MAT_DIALOG_DATA) public data: any){
 
-    }
-
-    ngOnInit() {
-        this.calculate();
-
+        }
+        ngOnInit() {
+            this.valores=this.calcular.digitSum(this.data[0],this.data[1],this.data[2]);
+            console.log(this.valores);
+            for (var _i = 0;_i < this.data[0]; _i++) {
+            this.calculos.push({
+                "t": _i,   
+                "anual":this.valores[0][_i],
+                "acumulada":this.valores[1][_i],  
+                "valor":this.valores[2][_i]
+            });
+        }
     }
 
     onNoClick(): void {
-        this.dialogRef.close({ data: 'false' });
-    }
-
-    calculate() {
-        let idEmpleado: number = parseInt(localStorage.getItem('IdEmpleado'));
-        this.restApi.getSedeXEmpleado(idEmpleado).subscribe((res1) => {
-            const myObjStr1 = JSON.stringify(res1)
-            const json1 = JSON.parse(myObjStr1);
-            const idSede = json1.data[0].IdSede;
-
-            this.restApi.getActivoAsignEmpleado(idEmpleado).subscribe((res2) => {
-                const myObjStr2 = JSON.stringify(res2)
-                const json2 = JSON.parse(myObjStr2);
-                this.restApi.getAllCostoInicialXEmpleado(idEmpleado).subscribe((res3) => {
-                    const myObjStr3 = JSON.stringify(res3)
-                    const json3 = JSON.parse(myObjStr3);
-                    this.calculos.push(json3.data[0]);
-                    this.restApi.getAllCostoInicialXEmpleadoProm(idEmpleado).subscribe((res4) => {
-                        const myObjStr4 = JSON.stringify(res4)
-                        const json4 = JSON.parse(myObjStr4);
-
-                        this.calculos.push(json4.data[0]);
-                        this.restApi.getAllValorResidualXEmpleado(idEmpleado).subscribe((res5) => {
-                            const myObjStr5 = JSON.stringify(res5)
-                            const json5 = JSON.parse(myObjStr5);
-                            this.calculos.push(json5.data[0]);
-
-                            this.restApi.getAllValorResidualXEmpleadoProm(idEmpleado).subscribe((res6) => {
-                                const myObjStr6 = JSON.stringify(res6)
-                                const json6 = JSON.parse(myObjStr6);
-                                this.calculos.push(json6.data[0]);
-
-                                this.restApi.getAllValorActivoActualXEmpleado(idEmpleado).subscribe((res7) => {
-                                    const myObjStr7 = JSON.stringify(res7)
-                                    const json7 = JSON.parse(myObjStr7);
-                                    this.calculos.push(json7.data[0]);
-
-                                    this.restApi.getAllValorActivoActualXEmpleadoProm(idEmpleado).subscribe((res8) => {
-                                        const myObjStr8 = JSON.stringify(res8)
-                                        const json8 = JSON.parse(myObjStr8);
-                                        this.calculos.push(json8.data[0]);
-
-                                        this.calculos = [{
-                                            "activosAsignados": json2.data[0].ActivosAsignados,
-                                            "costoInicial": json3.data[0].ActivosAsignados,
-                                            "costoInicialProm": json4.data[0].ActivosAsignados,
-                                            "valorResidual": json5.data[0].ActivosAsignados,
-                                            "valorResidualProm": json6.data[0].ActivosAsignados,
-                                            "valorActual": json7.data[0].ActivosAsignados,
-                                            "valorActualProm": json8.data[0].ActivosAsignados
-                                        }];
-                                    });
-                                });
-                            });
-
-                        });
-                    });
-                });
-            });
-        });
-    }
+        this.dialogRef.close({data:'false'});
+       }
 
 
 
 }
+
