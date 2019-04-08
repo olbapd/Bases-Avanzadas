c73285f8d00b4585cd011a46bb1a6cd5b08993de
JoseRIvera07 <ce.joserivera7@gmail.com>
2019-04-07 22:44:52 -0600

    somethig

--- Proyecto1/Database/spReportes.sql
+++ Proyecto1/Database/spReportes.sql
@@ -1,7 +1,7 @@
 --REPORTE #1 ADMINISTRADOR
 -----------------------------------------
 --Monto total de los activos asignados a la sede
-CREATE OR ALTER PROC [dbo].[getActivoAsign]
+CREATE OR ALTER PROC [dbo].[getAllActivoAsignXSede]
 	@IdSede int
 AS
 SET NOCOUNT ON
@@ -275,4 +275,92 @@ BEGIN
 		ROLLBACK TRANSACTION
 	END CATCH
 END
+GO
+
+----------------------------------------Gerente--------------------------------------------------------------------------------
+
+/*#1*/
+-------------------------------------------Monto total de los activos asignados a todas las sedes-------------------------------
+CREATE OR ALTER  PROC [dbo].[getTotalActivoBySedeGerente]
+AS 
+SELECT [Sede].Nombre, COUNT(*) ActivosAsignados
+FROM Activo
+INNER JOIN Sede ON [Activo].IdSede = [Sede].IdSede
+WHERE [Activo].IdEstado = 3
+GROUP BY [Sede].Nombre
+GO
+--monto total de los activos según el costo inicial
+
+CREATE OR ALTER PROC [dbo].[getAllCostoInicialXSedeGerente]
+AS
+
+SELECT [Sede].Nombre, SUM(Precio) AS SumaCostoInicial
+FROM Activo
+INNER JOIN Sede ON [Activo].IdSede = [Sede].IdSede
+GROUP BY [Sede].Nombre
+GO
+
+--el monto total de los activos según el valor residual
+
+CREATE OR ALTER PROC [dbo].[getAllValorResidualXSedeGerente]
+AS
+SELECT [Sede].Nombre, SUM(ValorResidual) AS SumaValorResidual
+FROM Activo
+INNER JOIN Sede ON [Activo].IdSede = [Sede].IdSede
+GROUP BY [Sede].Nombre
+GO
+
+-- monto total de los activos según el valor en libros a la fecha de la consulta
+CREATE OR ALTER PROC [dbo].[getAllValorActivoActualXSedeGerente]
+AS
+BEGIN
+	DECLARE
+
+	@IdSede int,
+	@PrecioInicialTotal int,
+	@ValorResidualTotal int,
+	@AnoR int,
+	@AnoA int,
+	@AnoT int
+
+
+	SELECT @ValorResidualTotal = SUM(ValorResidual) FROM Activo 
+	GROUP BY [Activo].IdSede
+
+	SELECT @PrecioInicialTotal = SUM(Precio)  FROM Activo
+	GROUP BY [Activo].IdSede
+
+	SELECT @AnoR =  year(FechaCompra)  FROM Activo
+	GROUP BY [Activo].IdSede
+
+	SELECT @AnoA =  year(GETDATE());
+
+	BEGIN TRAN
+	BEGIN TRY
+		SELECT [Sede].Nombre, 'ValorEnLibros' = @PrecioInicialTotal - (@AnoA-@AnoR)*((@PrecioInicialTotal-@ValorResidualTotal)/(@AnoA-@AnoR))
+		FROM Activo
+		INNER JOIN Sede ON [Activo].IdSede = [Sede].IdSede
+		GROUP BY [Sede].Nombre
+		COMMIT TRANSACTION
+	END TRY
+	BEGIN CATCH
+		SELECT ERROR_PROCEDURE() AS ErrorProcedimiento, ERROR_MESSAGE() AS TipoError
+		ROLLBACK TRANSACTION
+	END CATCH
+END
+GO
+
+-----------------REPORTE#2-------------------------------------------
+CREATE OR ALTER PROC [dbo].[getAllDetalleActivoAsignXSede]
+	@Ano int,
+	@VidaUtil int
+	
+AS
+	SELECT [Activo].Codigo, [Activo].Nombre, [Activo].Precio, [Activo].ValorResidual, [Categoria].Nombre, [Activo].FechaCompra,[Activo].VidaUtil,[Empleado].Nombre,[Sede].Nombre
+	FROM Activo
+	INNER JOIN Categoria ON [Activo].IdCategoria = [Categoria].IdCategoria
+	INNER JOIN Empleado ON [Activo].IdEmpleado = [Empleado].IdEmpleado
+	INNER JOIN Sede ON [Activo].IdSede = [Sede].IdSede
+	WHERE year([FechaCompra]) BETWEEN @Ano AND @Ano+@VidaUtil
+	GROUP BY [Sede].Nombre, [Activo].Codigo,[Activo].Nombre, [Activo].Precio, [Activo].ValorResidual, [Categoria].Nombre, [Activo].FechaCompra,[Activo].VidaUtil,[Empleado].Nombre
 GO
--- Proyecto1/Frontend/src/app/admin_view/dialogs/first_report/first_report.component.ts
+++ Proyecto1/Frontend/src/app/admin_view/dialogs/first_report/first_report.component.ts
@@ -7,17 +7,18 @@ import { updateComponent } from '../update_employee/update-employee.component';
 import { Calculo } from 'src/app/interfaces/calculo';
 import { Depreciation } from 'src/app/services/depreciation';
 import { freporte } from 'src/app/interfaces/freporte';
+import { CalculoAFirst } from 'src/app/interfaces/calculoAFirst';
 @Component({
     selector: 'first-report',
     templateUrl: './first_report.component.html',
     styleUrls: ['./first_report.component.css']
 })
 
-export class FirstReportComponent  implements OnInit {
+export class FirstReportComponent implements OnInit {
 
     page = 1;
     pageSize = 4;
-    calculos: number[] = [];//debe de inicializarse de lo contrario muestra vacio
+    calculos: CalculoAFirst[] = [];//debe de inicializarse de lo contrario muestra vacio
     valores;
     constructor(public calcular: Depreciation, private modalService: NgbModal, public restApi: RestApiService,
         private router: Router, private dialogRef: MatDialogRef<updateComponent>, @Inject(MAT_DIALOG_DATA) public data: any) {
@@ -25,6 +26,7 @@ export class FirstReportComponent  implements OnInit {
     }
 
     ngOnInit() {
+        this.calculate();
 
     }
 
@@ -34,40 +36,39 @@ export class FirstReportComponent  implements OnInit {
 
     calculate() {
         let idEmpleado: number = parseInt(localStorage.getItem('IdEmpleado'));
-        this.restApi.getSedeXEmpleado(idEmpleado).subscribe((res) => {
-            const myObjStr = JSON.stringify(res)
-            const json = JSON.parse(myObjStr);
-            const idSede = json.data[0].IdSede;
+        this.restApi.getSedeXEmpleado(idEmpleado).subscribe((res1) => {
+            const myObjStr1 = JSON.stringify(res1)
+            const json1 = JSON.parse(myObjStr1);
+            const idSede = json1.data[0].IdSede;
 
-            this.restApi.getAllActivoAsignXSede(idSede).subscribe((res) => {
-                const myObjStr = JSON.stringify(res)
-                const json = JSON.parse(myObjStr);
-                var count = Object.keys(json.data);
-                this.calculos.push(json.data[0]);
+            this.restApi.getAllActivoAsignXSede(idSede).subscribe((res2) => {
+                const myObjStr2 = JSON.stringify(res2)
+                const json2 = JSON.parse(myObjStr2);
+                this.restApi.getAllCostoInicialXSede(idSede).subscribe((res3) => {
+                    const myObjStr3 = JSON.stringify(res3)
+                    const json3 = JSON.parse(myObjStr3);
+                    this.calculos.push(json3.data[0]);
+                    this.restApi.getAllValorResidualXSede(idSede).subscribe((res4) => {
+                        const myObjStr4 = JSON.stringify(res4)
+                        const json4 = JSON.parse(myObjStr4);
+
+                        this.calculos.push(json4.data[0]);
+                        this.restApi.getAllValorActivoActualXSede(idSede).subscribe((res5) => {
+                            const myObjStr5 = JSON.stringify(res5)
+                            const json5 = JSON.parse(myObjStr5);
+                            this.calculos.push(json5.data[0]);
+
+                            this.calculos = [{
+                                "CantidadActivos": json2.data[0].ActivosAsignados,
+                                "TotalCostoInicial": json3.data[0].SumaCostoInicial,
+                                "TotalValorResidual": json4.data[0].SumaValorResidual,
+                                "TotalValorLibros": json5.data[0].ValorEnLibros
+                            }];
+                        });
+                    });
+                });
+
             });
-
-
-            this.restApi.getAllCostoInicialXSede(idSede).subscribe((res) => {
-                const myObjStr = JSON.stringify(res)
-                const json = JSON.parse(myObjStr);
-                var count = Object.keys(json.data);
-                this.calculos.push(json.data[0]);
-            });
-
-
-            this.restApi.getAllValorResidualXSede(idSede).subscribe((res) => {
-                const myObjStr = JSON.stringify(res)
-                const json = JSON.parse(myObjStr);
-                var count = Object.keys(json.data);
-                this.calculos.push(json.data[0]);
-            });
-            this.restApi.getAllValorActivoActualXSede(idSede).subscribe((res) => {
-                const myObjStr = JSON.stringify(res)
-                const json = JSON.parse(myObjStr);
-                var count = Object.keys(json.data);
-                this.calculos.push(json.data[0]);
-            });
-
         });
     }
 
