786cb394c959b929b4081be4b47c4e2426f49320
olbapd <pablod.garciab@gmail.com>
2019-05-04 01:12:29 -0600

    Created library main page.

--- Proyecto2/FRONTEND/src/app/pages/admin/admin.module.ts
+++ Proyecto2/FRONTEND/src/app/pages/admin/admin.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 //import { Ng2SmartTableModule } from 'ng2-smart-table';
 
+import { CommonModule } from '@angular/common';
+
 import { AdminRoutingModule, routedComponents } from './admin-routing.module';
 import { AdminService } from '../../services/admin.service';
 
 @NgModule({
   imports: [
+    CommonModule,
     AdminRoutingModule,
     //Ng2SmartTableModule,
   ],
--- Proyecto2/FRONTEND/src/app/pages/admin/main/main.component.ts
+++ Proyecto2/FRONTEND/src/app/pages/admin/main/main.component.ts
@@ -1,73 +1,33 @@
 import { Component } from '@angular/core';
-/*import { LocalDataSource } from 'ng2-smart-table';
 
-import { SmartTableService } from '../../../@core/data/smart-table.service';
+import { AdminService } from '../../../services/admin.service';
 
-import { ContactService } from '../../../@core/data/contact.service';
-import { sharedService } from '../../../@core/data/sharedService.service';
-*/
 @Component({
   selector: 'main',
   styleUrls: ['./main.component.scss'],
   templateUrl: './main.component.html',
 })
 export class MainComponent {
-  /*settings = {
-    
-    actions: {
-      add: false,
-      delete: false
-    },
-    edit: {
-      editButtonContent: '<i class="nb-edit"></i>',
-      saveButtonContent: '<i class="nb-checkmark"></i>',
-      cancelButtonContent: '<i class="nb-close"></i>',
-      confirmSave : true
-    },
-    columns: {
-      codigo_empresa: {
-        title: 'Codigo de Empresa',
-        type: 'string',
-      },
-      nomCliente: {
-        title: 'Nombre de Empresa',
-        type: 'string',
-      },
-      nombre: {
-        title: 'Nombre de Contacto',
-        type: 'string',
-      },
-      correo: {
-        title: 'Correo',
-        type: 'string',
-      },
-      numeroTelefono: {
-        title: 'Telefono',
-        type: 'number'
-      },      
-      observaciones: {
-        title: 'Observaciones',
-        type: 'string',
-      }
-    },
-  };*/
+  
+  libraries:any;
+  constructor(private adminServices:AdminService) {
+    this.libraries=[
+      {
+        code: "ABCDEF",
+        name: "Pancho Library",
+        country : "Costa Rica",
+        location: "Cartago",
+        number: 3331324354,
+        schedule: "L-V"
 
-  /*contactsJson : any; 
-  contactSource: LocalDataSource = new LocalDataSource();
-  */
-  constructor(/*private contactService: ContactService, private ss: sharedService*/) {
-    /*const data = this.contactService.getContactList(this.ss.getCurrentCiaId())
-      .subscribe( (data) =>  {
-        this.contactsJson=data.contacts;
-        this.contactSource.load(this.contactsJson);
-    });*/
+      },
+    ]
   }
 
-  /*onEditRow(event:any) {
-    if (window.confirm('Desea guardar estos cambios?')) {
-      event.confirm.resolve();
-    } else {
-      event.confirm.reject();
-    }
-  }*/
+  editLibrary(code){
+
+  }
+  deleteLibrary(code){
+
+  }
 }
