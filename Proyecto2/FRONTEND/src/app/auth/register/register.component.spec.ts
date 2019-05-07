f74e38fcc7c4b81577254f998e8af9bc010ad622
RicardoFino <ricar_fino@hotmail.com>
2019-05-06 23:19:25 -0600

    Update register.component.spec.ts

--- Proyecto2/FRONTEND/src/app/auth/register/register.component.spec.ts
+++ Proyecto2/FRONTEND/src/app/auth/register/register.component.spec.ts
@@ -1,6 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { RegisterComponent } from './resgister.component';
+import { RegisterComponent } from './register.component';
 
 describe('RegisterComponent', () => {
   let component: RegisterComponent;
