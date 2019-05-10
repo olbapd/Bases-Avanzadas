d9f76d2da4bdff23303008246bda7223b47d68a7
JoseRIvera <ce.joserivera7@gmail.com>
2019-05-10 15:14:58 -0600

    Data added to auth method

--- Proyecto2/BACKEND/controllers/auth.js
+++ Proyecto2/BACKEND/controllers/auth.js
@@ -1,25 +1,25 @@
 Usuario = require('../models/usuario');
 
 exports.view = function (req, res) {
-    Usuario.find({ 'usuario': req.body.usuario }, function (err, usuario) {
+    Usuario.find({ usuario: req.body.usuario }, function (err, usuario) {
 
 
         if (err) {
             res.json({
-                status: "error",
+                error:true,
                 message: err,
             });
             return
         }
         else if(req.body.contrasena == usuario[0].contrasena) {
             res.json({
-                status: "success"
+                status: true,
+                data:usuario
             });
             return
         }
         else {
-            res.json({status: "error",
-            message: err,});
+            res.json({});
             return
         }
         
