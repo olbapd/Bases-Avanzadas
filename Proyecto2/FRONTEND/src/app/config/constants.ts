47b5b58fbbf1795a8db6edba8cc4aca3a36eba03
olbapd <pablod.garciab@gmail.com>
2019-05-10 10:58:25 -0600

    Added config file.

---
+++ Proyecto2/FRONTEND/src/app/config/constants.ts
@@ -0,0 +1,5 @@
+export class Urls{
+	public static baseUrl:string="localhost:3000"
+}
+export  class Constants{
+}
--- Proyecto2/FRONTEND/src/app/services/client.service.ts
+++ Proyecto2/FRONTEND/src/app/services/client.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
+import { Urls } from '../config/constants';
 
 @Injectable()
 export class ClientService {
