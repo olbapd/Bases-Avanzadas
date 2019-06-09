474cb132fca724c761430df62195769d3465ee54
Kenneth Jeanpol Alvarado Mendez <ce.kalvarado@gmail.com>
2019-06-09 12:23:35 -0600

    Google Maps Api Integrated

--- Proyecto3/FRONTEND/src/app/app.module.ts
+++ Proyecto3/FRONTEND/src/app/app.module.ts
@@ -3,14 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import {AgmCoreModule} from '@agm/core';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AgmCoreModule.forRoot({
+      apiKey:'AIzaSyD9ZgAqbqfcy27LYpISyuIOZOpe0tMa4Fo'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
