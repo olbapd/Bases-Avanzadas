b4672d0b28de91ffdc2ebf3177b74337e9e76b9c
Kenneth Jeanpol Alvarado Mendez <ce.kalvarado@gmail.com>
2019-06-14 11:33:38 -0600

    Login Component Template added

--- Proyecto3/FRONTEND/src/app/auth/login/login.component.ts
+++ Proyecto3/FRONTEND/src/app/auth/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Injectable } from '@angular/core';
 import { Router, Params } from '@angular/router';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
+import { AuthService } from '../../services/login.service'; 
 
 @Component({
   selector: 'login',
@@ -11,24 +12,46 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class LoginComponent implements OnInit {
   loginForm: FormGroup;
   errorMessage: string = '';
-
+  
   constructor(
-    private router: Router,
-    private fb: FormBuilder) {
+  	  private router: Router,
+      private fb: FormBuilder,
+      private authService : AuthService){
 
     this.loginForm = this.fb.group({
       email: ['', Validators.required],
       password: ['', Validators.required]
     });
   }
-
+  
   ngOnInit() {
   }
 
-  login() {
+  login(){
     let username = this.loginForm.value.email;
     let password = this.loginForm.value.password;
+    this.authService.login(username,password)
+      .subscribe((result)=>{
+        if(result.error){
+          console.log(result.message);
+        }
+        if(result.status){
+          let user = result.data[0];
+          localStorage.setItem('user',JSON.stringify(user));
+          if(user.tipoUsuario==0){ //gerente
+            this.router.navigate(['/pages/admin/main']);  
+          }
+          else if(user.tipoUsuario==1){ //admin tienda
+            this.router.navigate(['/pages/book/main']);  
+          }
+          else if(user.tipoUsuario==2){ //cliente
+            this.router.navigate(['/pages/book/main']);  
+          }
+          else if(user.tipoUsuario==3){ //agente
+            this.router.navigate(['/pages/order/main']);  
+          }
+        }
+    });
   }
 }
-
 
