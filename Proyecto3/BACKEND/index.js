081b5e8a5e2b53fce758037dc77505337a1b0e21
RicardoFino <ricar_fino@hotmail.com>
2019-06-14 10:15:57 -0600

    Update index.js

--- Proyecto3/BACKEND/index.js
+++ Proyecto3/BACKEND/index.js
@@ -1,30 +1,34 @@
 // Import express
-let express = require('express'),upload = require('express-fileupload');
+let express = require('express')//,upload = require('express-fileupload');
 // Import Body parser
-let bodyParser = require('body-parser'),
-                 cors = require('cors');
+let bodyParser = require('body-parser');
+ //                cors = require('cors');
 // Import Mongoose
 let mongoose = require('mongoose');
 // Initialize the app
 let app = express();
 
-// Add headers
+
+
+/*// Add headers
 app.use(bodyParser.json({limit: "50mb"}));
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true, parameterLimit:50000 }));
 app.disable('etag'); // Avoid 304 requests
 app.use(cors()); // Enable request from any server -> npm install cors
 app.options('*', cors()) // include before other routes
 app.use(upload()); // Move to req.files any uploaded file
-// Import routes
+// Import routes*/
 let apiRoutes = require("./api-routes")
 // Configure bodyparser to handle post requests
 app.use(bodyParser.urlencoded({
     extended: true
 }));
 app.use(bodyParser.json());
+
+//
 // Connect to Mongoose and set connection variable
-const uri = 'mongodb+srv://admin:vt6xAFToIofyxyCS@cluster0-uhlxe.mongodb.net/';
-mongoose.connect(uri,{dbName: 'Proyecto2',useNewUrlParser: true});//ANTES DE CONECTAR RECORDAR AGREGAR IP A IPWHITELIST EN ATLAS
+const url = 'mongodb+srv://Proyecto3:BDAp3@bda-eiqv0.mongodb.net/test?retryWrites=true&w=majority';
+mongoose.connect(url,{dbName: 'Proyecto3',useNewUrlParser: true});//ANTES DE CONECTAR RECORDAR AGREGAR IP A IPWHITELIST EN ATLAS
 mongoose.set('useCreateIndex', true);
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
