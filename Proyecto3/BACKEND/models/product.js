c8f0c11c893b5c1ec9c2f9d190925cc0102b2ee1
RicardoFino <ricar_fino@hotmail.com>
2019-06-14 14:56:33 -0600

    Create product.js

---
+++ Proyecto3/BACKEND/models/product.js
@@ -0,0 +1,29 @@
+// Product models
+
+var mongoose = require('mongoose');
+// Setup schema
+var productSchema = mongoose.Schema({
+    _id:String,
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    description:{
+        type: String,
+        required: true
+    },
+    price:{
+        type: Number,
+        required: true
+    },
+    photo:{
+        type: String,
+        required: false
+    }
+},{ _id: false });
+// Export Product model
+var Product = module.exports = mongoose.model('Product', productSchema);
+module.exports.get = function (callback, limit) {
+    Product.find(callback).limit(limit);
+}
