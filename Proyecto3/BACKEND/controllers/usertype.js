6d4923990800b7f065e892049b52cee5c298bcdc
RicardoFino <ricar_fino@hotmail.com>
2019-06-14 23:13:55 -0600

    Create usertype.js

---
+++ Proyecto3/BACKEND/controllers/usertype.js
@@ -0,0 +1,80 @@
+//userType Controller
+
+UserType = require('../models/usertype');
+
+
+// Handle view usertype
+exports.index = function (req, res) {
+    UserType.get(function (err, userstype) {
+        if (err) {
+            res.json({
+                status: "error",
+                message: err,
+            });
+            return
+        }
+        res.json({
+            status: "success",
+            data: userstype
+        });
+    });
+};
+
+//Handle create usertype
+exports.new = function (req, res) {
+    var usertype = new UserType();
+    usertype._id = req.body.id;
+    usertype.name = req.body.name;
+
+    // save the contact and check for errors
+    usertype.save(function (err) {
+        if (err) {
+            res.json({
+                status: "error",
+                message: err,
+            });
+            return
+        }
+        res.json({
+            status: "success",
+            data: usertype,
+            message:"UserType created"
+        });
+    });
+};
+
+//Handle view usertype by id
+exports.view = function (req, res) {
+    UserType.find({ '_id': req.params.usertype_id }, function (err, usertype) {
+
+        if (err) {
+            res.json({
+                status: "error",
+                message: err,
+            });
+            return
+        }
+        res.json({
+            status: "success",
+            data: usertype
+        });
+    });
+};
+
+
+// Handle delete usertype
+exports.delete = function (req, res) {
+    UserType.deleteOne({_id: req.params.usertype_id}, function (err, usertype) {
+        if (err) {
+            res.json({
+                status: "error",
+                message: err,
+            });
+            return
+        }
+        res.json({
+            status: "success",
+            message:"UserType deleted"
+        });
+    });
+};
